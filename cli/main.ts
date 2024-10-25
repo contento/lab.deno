740fdc14a306ad145ec7967e3efb1f8492019850
Gonzalo Contento <3741250+contento@users.noreply.github.com>
2024-10-25 10:43:53 -0400

    adding more tests

--- cli/main.ts
+++ cli/main.ts
@@ -4,5 +4,5 @@ export function add(a: number, b: number): number {
 
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  console.log("Add 1 + 2 + 3 =", add(2, 3));
+  console.log("Add 1 + 2 + 3 =", add(1, add(2, 3)));
 }
--- cli/main_test.ts
+++ cli/main_test.ts
@@ -1,6 +1,26 @@
 import { assertEquals } from "@std/assert";
 import { add } from "./main.ts";
 
-Deno.test(function addTest() {
+Deno.test("addTestSimple", () => {
   assertEquals(add(2, 3), 5);
+});
+
+Deno.test("addTestComposite", () => {
+  assertEquals(add(1, add(2, 3)), 6);
+});
+
+Deno.test("addTestNegative", () => {
+  assertEquals(add(-1, -1), -2);
+});
+
+Deno.test("addTestZero", () => {
+  assertEquals(add(0, 0), 0);
+});
+
+Deno.test("addTestLargeNumbers", () => {
+  assertEquals(add(1000, 2000), 3000);
+});
+
+Deno.test("addTestMixed", () => {
+  assertEquals(add(-5, 10), 5);
 });
